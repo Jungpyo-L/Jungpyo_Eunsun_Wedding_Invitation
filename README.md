8fd310a1df38c935eed428f3f51e2c25e933eb0e
Jungpyo-L <ljp0187@gmail.com>
2022-04-25 14:45:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 이정표와 장은선의 결혼식 모바일 청첩장
+
+이 repository는 결혼식 초대를 위한 모바일청첩장입니다.
+
+
+![메인사진](https://github.com/Jungpyo-L/Jungpyo_Eunsun_Wedding_Invitation/blob/master/docs/images/pic1_main.jpg)
+
+## 예식정보
+
+* 날짜 : 2022년 5월 28일 저녁 7시 30분
+* 장소 : 서울 강남구 도곡로99길 16 트라디노이
+* 청첩장 url : https://Jungpyo-L.github.io/Jungpyo_Eunsun_Wedding/
---
+++ docs/js/main.js
@@ -0,0 +1,165 @@
+$(window).resize(function(){resizeYoutube();});
+$(function(){resizeYoutube();});
+function resizeYoutube(){ $("iframe").each(function(){ if( /^https?:\/\/www.youtube.com\/embed\//g.test($(this).attr("src")) ){ $(this).css("width","100%"); $(this).css("height",Math.ceil( parseInt($(this).css("width")) * 480 / 854 ) + "px");} }); }
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('#go-to-top').click(function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+});
+
+// account
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+d = new Date();
+$(document).ready(function () {
+    $(".grm_account_bank").html("NH농협");
+    $(".brd_account_bank").html("신한");
+    $(".grm_account_num").text("1234");
+    $(".brd_account_num").text("1324");
+    $(".GroomAccountName").text("이상하");
+    $(".BrideAccountName").text("장문석");
+    $(".GroomAccountRelation").text("혼주");
+    $(".BrideAccountRelation").text("혼주");
+
+    $(".grm_account_bank2").html("우리은행");
+    $(".brd_account_bank2").html("신한은행");
+    $(".grm_account_num2").text("1002 943 523660");
+    $(".brd_account_num2").text("110 264 079377");
+    $(".GroomAccountName2").text("이정표");
+    $(".BrideAccountName2").text("장은선");
+    $(".GroomAccountRelation2").text("신랑");
+    $(".BrideAccountRelation2").text("신부");
+
+
+    if ($.trim("혼주") != "") {
+        $(".GroomAccountRelationArea").show();
+    } else {
+        $(".GroomAccountRelationArea").hide();
+    }
+
+    if ($.trim("혼주") != "") {
+        $(".GroomAccountRelationArea2").show();
+    } else {
+        $(".GroomAccountRelationArea2").hide();
+    }
+
+    if ($.trim("혼주") != "") {
+        $(".BrideAccountRelationArea").show();
+    } else {
+        $(".BrideAccountRelationArea").hide();
+    }
+
+    if ($.trim("신부") != "") {
+        $(".BrideAccountRelationArea2").show();
+    } else {
+        $(".BrideAccountRelationArea2").hide();
+    }
+
+
+    if ($.trim("박종국") != "") {
+        $(".GroomAccountNameArea").show();
+    } else {
+        $(".GroomAccountNameArea").hide();
+    }
+
+    if ($.trim("박현준") != "") {
+        $(".GroomAccountNameArea2").show();
+    } else {
+        $(".GroomAccountNameArea2").hide();
+    }
+
+    if ($.trim("임헌호") != "") {
+        $(".BrideAccountNameArea").show();
+    } else {
+        $(".BrideAccountNameArea").hide();
+    }
+
+    if ($.trim("임은민") != "") {
+        $(".BrideAccountNameArea2").show();
+    } else {
+        $(".BrideAccountNameArea2").hide();
+    }
+
+
+    if ("Y" == "Y" || "Y" == "Y" || "Y" == "Y" || "Y" == "Y") {
+        $(".remittance").show();
+        $("#remittance").show();
+    } else {
+        $(".remittance").hide();
+        $("#remittance").hide();
+    }
+
+    if ("Y" == "Y" || "Y" == "Y") {
+        $(".btn_bx_1").show();
+        $("#btn_bx_1").show();
+
+        if ("Y" == "Y") {
+            $(".re_grm_1").show();
+        } else {
+            $(".re_grm_1").hide();
+        }
+        if ("Y" == "Y") {
+            $(".re_grm_2").show();
+        } else {
+            $(".re_grm_2").hide();
+        }
+    } else {
+        $(".btn_bx_1").hide();
+        $("#btn_bx_1").hide();
+    }
+
+    if ("Y" == "Y" || "Y" == "Y") {
+        $(".btn_bx_2").show();
+        $("#btn_bx_2").show();
+
+        if ("Y" == "Y") {
+            $(".re_brd_1").show();
+        } else {
+            $(".re_brd_1").hide();
+        }
+        if ("Y" == "Y") {
+            $(".re_brd_2").show();
+        } else {
+            $(".re_brd_2").hide();
+        }
+
+    } else {
+        $(".btn_bx_2").hide();
+        $("#btn_bx_2").hide();
+    }
+});
+
+//초기화
+$('#remittance_pop_h').hide();
+$('#remittance_pop_w').hide();
+
+// 팝업
+$(function () {
+    $('.h_btn').click(function () {
+        $('#remittance_pop_h').show();
+        //$('.remittance_pop_h').show();
+    });
+    $('.w_btn').click(function () {
+        $('#remittance_pop_w').show();
+        //$('.remittance_pop_w').show();
+    });
+    $('.close_btn').click(function () {
+        $('#remittance_pop_h, #remittance_pop_w').hide();
+        //$('.remittance_pop_h, .remittance_pop_w').hide();
+    });
+});
+
+//복사
+function copyToClipboard(elementId) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("복사되었습니다.");
+}
